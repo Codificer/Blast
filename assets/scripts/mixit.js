74a8e93c483fd29a48829aafdad1157b10ee070b
Codificer <codificer@mail.ru>
2021-08-10 22:20:11 +0500

    подготовлено к перемешиванию

---
+++ assets/scripts/mixit.js
@@ -0,0 +1,38 @@
+//there will be restart code for button
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        button:cc.Button,
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.node.on('click', this.callback, this);
+    },
+    callback: function (button) {//перемешивание
+        /*var selfy=button;
+        var conf=selfy.node.parent.getComponent("config");
+        */
+    },
+
+    start () {
+    },
+    // update (dt) {},
+});
